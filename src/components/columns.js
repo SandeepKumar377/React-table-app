e0b0a0dd196402f4bec68f9a1a2b63a6357130a8
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-28 01:54:48 +0530

    Header Groups

--- src/components/columns.js
+++ src/components/columns.js
@@ -13,7 +13,7 @@ export const COLUMNS = [
         Header: 'Last Name',
         Footer: 'Last Name',
         accessor: 'last_name'
-    },    
+    },
     {
         Header: 'Date of Birth',
         Footer: 'Date of Birth',
@@ -28,5 +28,50 @@ export const COLUMNS = [
         Header: 'Phone',
         Footer: 'Phone',
         accessor: 'phone'
+    }
+]
+
+export const GROUPED_COLUMNS = [
+    {
+        Header: 'Id',
+        Footer: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Name',
+        Footer: 'Name',
+        columns: [
+            {
+                Header: 'First Name',
+                Footer: 'First Name',
+                accessor: 'first_name'
+            },
+            {
+                Header: 'Last Name',
+                Footer: 'Last Name',
+                accessor: 'last_name'
+            },
+        ]
+    },
+    {
+        Header: 'Info',
+        Footer: 'Info',
+        columns: [
+            {
+                Header: 'Date of Birth',
+                Footer: 'Date of Birth',
+                accessor: 'date_of_birth'
+            },
+            {
+                Header: 'Country',
+                Footer: 'Country',
+                accessor: 'country'
+            },
+            {
+                Header: 'Phone',
+                Footer: 'Phone',
+                accessor: 'phone'
+            }
+        ]
     }
 ]
