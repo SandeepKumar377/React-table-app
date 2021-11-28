9f9a16916a0b91c38f80acad8a84ed328be61eca
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-29 03:28:24 +0530

    Global Filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 // import BasicTable from './components/BasicTable';
-import SortingTable from './components/SortingTable';
+// import SortingTable from './components/SortingTable';
+import FilteringTable from './components/FilteringTable';
 
 function App() {
   return (
     <div className="App">
-      <SortingTable/>
+      <FilteringTable/>
+      {/* <SortingTable/> */}
       {/* <BasicTable /> */}
     </div>
   );
